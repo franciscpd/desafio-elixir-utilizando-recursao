bffdc8c3e84e98c34f20fc1c496db62409f2aaeb
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-03-05 20:28:14 -0300

    feat: resolução desafio módulo 1

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], acc) do
+    acc
+  end
+
+  defp count([_head | tail], acc) do
+    count(tail, acc + 1)
   end
 end
